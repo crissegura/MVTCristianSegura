94fbe3c094a6790342c4391941d7691cd65eb65a
crissegura00 <crisemanuelsegura@gmail.com>
2022-08-25 10:33:33 -0300

    primer commit

--- AppMVTCristianSegura/models.py
+++ AppMVTCristianSegura/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Familiares(models.Model):
     nombre = models.CharField(max_length=40)
     edad = models.IntegerField()
+    nacimiento = models.CharField(max_length=40)
+    
--- AppMVTCristianSegura/views.py
+++ AppMVTCristianSegura/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 from .models import Familiares
 
 def familiar(request):
-    familiar1 = Familiares(nombre='Juan Segura',edad=34)
+    familiar1 = Familiares(nombre='Juan Segura',edad=34,nacimiento='02/10/88')
     familiar1.save()
-    familiar2 = Familiares(nombre='Roman Segura',edad=24)
+    familiar2 = Familiares(nombre='Roman Segura',edad=24,nacimiento='05/06/98')
     familiar2.save()
-    familiar3 = Familiares(nombre='Martin Segura',edad=14)
+    familiar3 = Familiares(nombre='Martin Segura',edad=14,nacimiento='10/11/08')
     familiar3.save()
-    texto1 = f"Familiar: {familiar1.nombre} Edad: {familiar1.edad} "
-    texto2 = f"Familiar: {familiar2.nombre} Edad:{familiar2.edad} "
-    texto3 = f"Familiar: {familiar3.nombre} Edad: {familiar3.edad} "
+    texto1 = f"Familiar: {familiar1.nombre} Edad: {familiar1.edad} Fecha de nacimiento: {familiar1.nacimiento}  "
+    texto2 = f"Familiar: {familiar2.nombre} Edad:{familiar2.edad} Fecha de nacimiento: {familiar2.nacimiento} "
+    texto3 = f"Familiar: {familiar3.nombre} Edad: {familiar3.edad} Fecha de nacimiento: {familiar3.nacimiento} "
     return HttpResponse(texto1+' // '+texto2+' // '+texto3) 
 
